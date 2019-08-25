452f968787c7af632ab32356ebc43dc10b3d16b6
Raphael Baruffi <raphabaruffi@gmail.com>
2019-08-25 19:12:33 -0300

    Adicionados sprites

--- jogo.py
+++ jogo.py
@@ -1,7 +1,6 @@
 # imports
 
 import pygame
-from pygame.locals import *
 
 # inicio da engine
 
@@ -10,21 +9,20 @@ pygame.init()
 # recursos
 
 tela = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('Jogo da Velha')
-
 relogio = pygame.time.Clock()
+
+xises = 0
+bolinhas = 0
+
+#configuracoes
+
+pygame.display.set_caption('Jogo da Velha')
 
 # desenhaveis
 
-velha=[
-{'posicao': (0, 200), 'superficie': pygame.Surface((600, 10))},
-{'posicao': (0, 400), 'superficie': pygame.Surface((600, 10))},
-{'posicao': (200, 0), 'superficie': pygame.Surface((10, 600))},
-{'posicao': (400, 0), 'superficie': pygame.Surface((10, 600))},
-]
-
-xis = []
-bolinha = []
+velha = pygame.image.load("velha.png").convert_alpha()
+xis = pygame.image.load("xis.png").convert_alpha()
+bolinha = pygame.image.load("bolinha.png").convert_alpha()
 
 # funcoes
 
@@ -32,18 +30,19 @@ def tique():
     relogio.tick(60)
 
 def atualiza():
-
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
 
 def desenha():
-
     tela.fill((0,0,0))
+    tela.blit(velha, (0, 0))
 
-    for linha in velha:
-        linha.get('superficie').fill((255,255,255))
-        tela.blit(linha.get('superficie'), linha.get('posicao'))
+    for i in range(0, bolinhas):
+        tela.blit(bolinha, (0, 0))
+
+    for i in range(0, xises):
+        tela.blit(xis, (0, 0))
 
 def exibe():
     pygame.display.update()
@@ -51,7 +50,6 @@ def exibe():
 # loop do jogo
 
 while True:
-
     tique()
     atualiza()
     desenha()
