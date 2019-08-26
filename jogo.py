5bad3046f75f17aea29e4d51cf5ba269afb5b2f7
Raphael Baruffi <raphabaruffi@gmail.com>
2019-08-25 21:42:39 -0300

    jogo quase completo

--- jogo.py
+++ jogo.py
@@ -6,17 +6,26 @@ import pygame
 
 pygame.init()
 
-# recursos
-
-tela = pygame.display.set_mode((600, 600))
-relogio = pygame.time.Clock()
+# listas
 
 xises = []
 bolinhas = []
-posicao_selecao = (0, 0)
+turnos = []
+
+linhas = [
+    ((0, 0), (0, 200), (0, 400)),
+    ((200, 0), (200, 200), (200, 400)),
+    ((400, 0), (400, 200), (400, 400)),
+    ((0, 0), (200, 0), (400, 0)),
+    ((0, 200), (200, 200), (400, 200)),
+    ((0, 400), (200, 400), (400, 400)),
+    ((0, 0), (200, 200), (400, 400)),
+    ((400, 0), (200, 200), (0, 400)),
+]
 
 #configuracoes
 
+tela = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('Jogo da Velha')
 
 # desenhaveis
@@ -25,9 +34,16 @@ velha = pygame.image.load("velha.png").convert_alpha()
 xis = pygame.image.load("xis.png").convert_alpha()
 bolinha = pygame.image.load("bolinha.png").convert_alpha()
 
+xis_transparente = xis.copy()
+xis_transparente.fill((255, 255, 255, 128), None, pygame.BLEND_RGBA_MULT)
+
+bolinha_transparente = bolinha.copy()
+bolinha_transparente.fill((255, 255, 255, 128), None, pygame.BLEND_RGBA_MULT)
+
 # colidiveis
 
 campo = pygame.Surface((200, 200))
+
 campos = [
     (campo, (0, 0)),   (campo, (200, 0)),   (campo, (400, 0)),
     (campo, (0, 200)), (campo, (200, 200)), (campo, (400, 200)),
@@ -36,33 +52,81 @@ campos = [
 
 # funcoes
 
-def tique():
-    relogio.tick(60)
+def jogador_venceu():
+    for linha in linhas:
+        for celula in linha:
+            value = True
+            if not xises.__contains__(celula):
+                value = False
+                break
+    return value
+
+def computador_venceu():
+    for linha in linhas:
+        for celula in linha:
+            value = True
+            if not bolinhas.__contains__(celula):
+                value = False
+                break
+    return value
+
+def empatou():
+    if turnos.__len__() == 9:
+        return True
+
+def reinicia():
+    xises.clear()
+    bolinhas.clear()
+    turnos.clear()
 
 def atualiza():
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             pygame.quit()
+
+        
         if event.type == pygame.MOUSEBUTTONDOWN:
             for campo in campos:
                 if campo[0].get_rect(topleft=campo[1]).collidepoint(pygame.mouse.get_pos()):
-                    if not bolinhas.__contains__(campo[1]):
-                        if xises.__contains__(campo[1]):
-                            xises.remove(campo[1])
-                        bolinhas.append(campo[1])
-                    else:
-                        bolinhas.remove(campo[1])
-                        xises.append(campo[1])
+                    if not xises.__contains__(campo[1]) and not bolinhas.__contains__(campo[1]):
+
+                        if turnos.__len__() % 2 == 0:
+                            xises.append(campo[1])
+                        else:
+                            bolinhas.append(campo[1])
+
+                        turnos.append((xises, bolinhas))
+    
+    if jogador_venceu():
+        print('Vitoria para o jogador!')
+        reinicia()
+
+    if computador_venceu():
+        print('Vitoria para o computador!')
+        reinicia()
+
+    if empatou():
+        print('Empate!')
+        reinicia()
 
 def desenha():
     tela.fill((0,0,0))
+
     tela.blit(velha, (0, 0))
 
-    for posicao in bolinhas:
-        tela.blit(bolinha, posicao)
+    for campo in campos:
+        if campo[0].get_rect(topleft=campo[1]).collidepoint(pygame.mouse.get_pos()):
+            if turnos.__len__() % 2 == 0:
+                tela.blit(xis_transparente, campo[1])
+            else:
+                tela.blit(bolinha_transparente, campo[1])
 
     for posicao in xises:
         tela.blit(xis, posicao)
+
+    for posicao in bolinhas:
+        tela.blit(bolinha, posicao)
 
 def exibe():
     pygame.display.update()
@@ -70,7 +134,6 @@ def exibe():
 # loop do jogo
 
 while True:
-    tique()
     atualiza()
     desenha()
     exibe()
